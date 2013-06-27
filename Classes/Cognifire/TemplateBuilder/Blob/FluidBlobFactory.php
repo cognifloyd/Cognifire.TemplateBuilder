297622c2d0961b8762efc7461a92ce2d63531ee6
Jacob Floyd <cognifloyd@gmail.com>
2013-06-26 23:00:03 -0600

    [TASK] Document @api and rewrite some docblocks

---
+++ Classes/Cognifire/TemplateBuilder/Blob/FluidBlobFactory.php
@@ -0,0 +1,26 @@
+<?php
+namespace Cognifire\TemplateBuilder\Blob;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package                          *
+ * "Cognifire.TemplateBuilder".                                           *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+
+use TYPO3\Flow\Annotations as Flow;
+use Cognifire\BuilderFoundation\Blob\AbstractBlobFactory;
+
+/**
+ * This should manipulate fluid files by using the fluid parser to modify the abstract syntax tree.
+ *
+ * @Flow/Scope("Singleton")
+ */
+class FluidBlobFactory extends AbstractBlobFactory {
+
+}
